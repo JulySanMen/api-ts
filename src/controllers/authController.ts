a2d45064a0be93b1fffa2f9ab82539dad37bf237
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-05-16 07:45:52 -0600

    refactor register method in AuthController to simplify response structure and improve error handling

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { UserRepository } from "../repositories/userRepository";
 import { AuthService } from "../services/authService";
+import { AnyArray } from "mongoose";
 
 const authService = new AuthService(new UserRepository());
 
@@ -8,16 +9,13 @@ export class AuthController {
     static async register (req:Request, res:Response){
         try{
             const token = await authService.register(req.body);
-            res.status(201).json({
-                status: "success",
-                token,
-            });
-        }catch(error){
-            console.error('Error in register:', error);
-            res.status(500).json({
-                status: 'error',
-                message: 'Internal Server Error',
-                });
+            res.json({token});
+        }catch(error:any){
+            res.status(400).json({message: error.message || "Datos no validos"});
         }
+    }
+    static async login (req:Request, res:Response){
+       
+
     }
 }
