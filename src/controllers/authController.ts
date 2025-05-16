d7fd8ee05ba1fa854a3be44c36b572f184e6c5e4
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-05-16 07:49:09 -0600

    implement error handling in login method of AuthController

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -15,7 +15,13 @@ export class AuthController {
         }
     }
     static async login (req:Request, res:Response){
-       
+        try{
+            const token = authService.login(req.body);//{email, password}
+            res.json({token});
+        }catch(error:any){
+            res.status(400).json({message: error.message || "Credenciales no validas"});
+        }
+    }
 
     }
 }
